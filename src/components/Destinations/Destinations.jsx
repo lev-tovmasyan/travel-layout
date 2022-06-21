2a37393916e9da78c4e1a5a092e5b50be92e9f86
MKrtchian <haykmk97@gmail.com>
2022-06-21 10:36:03 -0400

    destination

--- src/components/Destinations/Destinations.jsx
+++ src/components/Destinations/Destinations.jsx
@@ -3,8 +3,48 @@ import s from './Destinations.module.scss';
 
 function Destinations() {
   return (
-    <div>
-      <div>Destinations</div>
+    <div className={s.Destinations}>
+      <div className={s.destinationTitle}>
+        <h2 style={{ fontSize: 35 }}>
+          Our Popular Destinations
+        </h2>
+      </div>
+      <div className={s.destinationContent}>
+        <div className={s.destinationGlobus} />
+        <div className={s.destinationItems} >
+          <div className={s.destinationItem}>
+            <div className={s.itemImg1} />
+            <div className={s.itemCOntent}>
+              <div className={s.itemInnerContent}>
+                <h3>Thailand</h3>
+                <h3>20+ Spots</h3>
+                <h3>2D & 3N</h3>
+              </div>
+            </div>
+          </div>
+          <div className={s.destinationItem}>
+            <div className={s.itemImg2} />
+            <div className={s.itemCOntent}>
+              <div className={s.itemInnerContent}>
+                <h3>Indonesia</h3>
+                <h3>25+ Spots</h3>
+                <h3>3D & 2N</h3>
+              </div>
+            </div>
+          </div>
+          <div className={s.destinationItem}>
+            <div className={s.itemImg3} />
+            <div className={s.itemCOntent}>
+              <div className={s.itemInnerContent}>
+                <h3>New Zealand</h3>
+                <h3>20+ Spots</h3>
+                <h3>2D & 3N</h3>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
